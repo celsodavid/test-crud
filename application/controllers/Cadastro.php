a2ce4cf91ac69c712f87f64c9c37c90fe05e8119
celsodavid <celunico43@gmail.com>
2019-01-11 00:16:55 -0200

    Fluxo de insercao, aplicacao de validacao

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -18,6 +18,29 @@ class Cadastro extends CI_Controller
         $this->load->view('base/footer');
 	}
 
+    public function adicionar()
+    {
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('nome', 'Nome', 'required');
+        $this->form_validation->set_rules('email', 'E-mail', 'required|valid_email|is_unique[cadastro.email]');
+        $this->form_validation->set_rules('senha', 'Senha', 'required|min_length[6]');
+        $this->form_validation->set_rules('confirmar_senha', 'Confirmar Senha', 'required|min_length[6]|matches[senha]');
+        $this->form_validation->set_rules('telefone', 'Telefone', 'required|callback_valida_telefone');
+        if ($this->form_validation->run() == false) {
+            $this->index();
+        }
+        else {
+            exit('cadastrar');
+        }
+	}
+
+	public function valida_telefone()
+    {
+        $this->form_validation->set_message("valida_telefone", "Este telefone é inválido");
+        return false;
+    }
+
     public function ver($id)
     {
         $data_header['menu'] = 'cad';
--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -1,38 +1,47 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="row marketing">
     <div class="col-lg-12">
-        <form>
+        <?php echo validation_errors(); ?>
+        <form method="post" action="<?php echo base_url('cadastro/adicionar') ?>" id="form_cadastro">
             <div class="form-group">
                 <label for="exampleInputNome">Nome</label>
-                <input type="text" class="form-control" id="exampleInputNome" placeholder="Nome">
+                <input type="text" class="form-control" id="exampleInputNome" name="nome"
+                       placeholder="Nome" value="<?php echo set_value('nome') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="E-mail">
+                <input type="email" class="form-control" id="exampleInputEmail1" name="email"
+                       placeholder="E-mail" value="<?php echo set_value('email') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Senha</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="senha"
+                       placeholder="Senha" value="<?php echo set_value('senha') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputPasswordConfirm">Confirmar Senha</label>
-                <input type="password" class="form-control" id="exampleInputPasswordConfirm" placeholder="Confirmar Senha">
+                <input type="password" class="form-control" id="exampleInputPasswordConfirm" name="confirmar_senha"
+                       placeholder="Confirmar Senha" value="<?php echo set_value('confirmar_senha') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputPhone">Telefone</label>
-                <input type="text" class="form-control" id="exampleInputPhone" placeholder="Telefone">
+                <input type="text" class="form-control" id="exampleInputPhone" name="telefone"
+                       placeholder="Telefone" value="<?php echo set_value('telefone') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputRg">RG</label>
-                <input type="text" class="form-control" id="exampleInputRg" placeholder="RG">
+                <input type="text" class="form-control" id="exampleInputRg" name="rg"
+                       placeholder="RG" value="<?php echo set_value('rg') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputCep">Cep</label>
-                <input type="text" class="form-control" id="exampleInputCep" placeholder="Cep">
+                <input type="text" class="form-control" id="exampleInputCep" name="cep"
+                       placeholder="Cep"  value="<?php echo set_value('cep') ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputAddress">Endereço</label>
-                <input type="text" class="form-control" id="exampleInputAddress" placeholder="Endereço">
+                <input type="text" class="form-control" id="exampleInputAddress" name="endereco"
+                       placeholder="Endereço"  value="<?php echo set_value('endereco') ?>">
             </div>
             <button type="submit" class="btn btn-default">Cadastrar</button>
         </form>
